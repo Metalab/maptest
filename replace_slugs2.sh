92bb01b8b4adb7f3e6ccc0661034b0b51c7571bd
jstsmthrgk <38539927+jstsmthrgk@users.noreply.github.com>
2021-12-17 17:20:29 +0000

    Deploying to gh-pages from master @ e37b694a0850461462c2782eea18535cd8d5be94 🚀

---
+++ replace_slugs2.sh
@@ -0,0 +1,13 @@
+for f in $@
+do
+	tmp=$(mktemp)
+	jq -ce '.properties += [{"name":"apiUrl","type":"string","value":"https://play.aaaaaaaaaaaadventu.re"}] | (.layers[] | select(.properties != null and .properties[].name=="jitsiRoom") | .properties) += [{"name":"jitsiUrl","type":"string","value":"meet.jit.si"}]' $f >$tmp
+	mv $tmp $f
+done
+sed -i \
+-e 's/"world:\\\/\\\/devlol\\\/\(.*\)"/"\\\/_\\\/global\\\/devlol-systems.pages.devlol.org\\\/vspace-workadv\\\/\1"/g' \
+-e 's/"world:\\\/\\\/grandgarage\\\/\(.*\)"/"\\\/_\\\/global\\\/grand-garage.github.io\\\/gg-workadventure\\\/\1"/g' \
+-e 's/"world:\\\/\\\/Metalab\\\/\(.*\)"/"\\\/_\\\/global\\\/metalab.github.io\\\/maptest\\\/\1"/g' \
+-e 's/"world:\\\/\\\/realraum\\\/\(.*\)"/"\\\/_\\\/global\\\/realraum.github.io\\\/workadventur3space\\\/\1"/g' \
+-e 's/"world:\\\/\\\/steyr-werke\\\/\(.*\)"/"\\\/_\\\/global\\\/steyr-werke.gitlab.io\\\/workadventure-stw\\\/\1"/g' \
+$@
