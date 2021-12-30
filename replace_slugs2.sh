3318f6444c8ef7760be6d60f91c4d265f367f135
jstsmthrgk <38539927+jstsmthrgk@users.noreply.github.com>
2021-12-30 12:24:28 +0000

    Deploying to gh-pages from master @ 47f8aaafe16e378a7afb6ff3310afd5b3c2fc750 🚀

--- replace_slugs2.sh
+++ replace_slugs2.sh
@@ -6,6 +6,7 @@ sed -i \
 -e 's/"world:\\\/\\\/realraum\\\/\(.*\)"/"\\\/_\\\/global\\\/realraum.github.io\\\/workadventur3space\\\/\1"/g' \
 -e 's/"world:\\\/\\\/steyr-werke\\\/\(.*\)"/"\\\/_\\\/global\\\/steyr-werke.gitlab.io\\\/workadventure-stw\\\/\1"/g' \
 -e 's/"world:\\\/\\\/chaoszone\\\/\(.*\)"/"\\\/_\\\/global\\\/raw.githubusercontent.com\\\/die3ungleichen\\\/2D-Welt-thueringen\\\/main\\\/\1"/g' \
+-e 's/"world:\\\/\\\/ccc-basel\\\/\(.*\)"/"\\\/_\\\/global\\\/rc3.ccc-basel.ch\\\/\1"/g' \
 $@
 for f in $@
 do
@@ -15,8 +16,6 @@ do
 			[{"name":"apiUrl","type":"string","value":"https://play.aaaaaaaaaaaadventu.re"}]
 		| (.layers[] | select(.properties != null and .properties[].name=="jitsiRoom") | .properties) +=
 			[{"name":"jitsiUrl","type":"string","value":"meet.jit.si"}]
-		| (.layers[].properties[]? | select(.name == "exitUrl" and (.value | startswith("world://")))| .name) |=
-			"badExitUrl"
 		' $f >$tmp
 	mv $tmp $f
 done
