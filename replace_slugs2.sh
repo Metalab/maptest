2add36f1ef639a8a80e84f00ee357f2a20fa2582
jstsmthrgk <38539927+jstsmthrgk@users.noreply.github.com>
2021-12-30 09:14:47 +0000

    Deploying to gh-pages from master @ 47f8aaafe16e378a7afb6ff3310afd5b3c2fc750 🚀

--- replace_slugs2.sh
+++ replace_slugs2.sh
@@ -1,4 +1,5 @@
 sed -i \
+-e 's/"world:\\\/\\\/cccsbg\\\/\(.*\)"/"\\\/_\\\/global\\\/finga.github.io\\\/rc3-cccsbg\\\/\1"/g' \
 -e 's/"world:\\\/\\\/devlol\\\/\(.*\)"/"\\\/_\\\/global\\\/devlol-systems.pages.devlol.org\\\/vspace-workadv\\\/\1"/g' \
 -e 's/"world:\\\/\\\/grandgarage\\\/\(.*\)"/"\\\/_\\\/global\\\/grand-garage.github.io\\\/gg-workadventure\\\/\1"/g' \
 -e 's/"world:\\\/\\\/Metalab\\\/\(.*\)"/"\\\/_\\\/global\\\/metalab.github.io\\\/maptest\\\/\1"/g' \
@@ -9,6 +10,13 @@ $@
 for f in $@
 do
 	tmp=$(mktemp)
-	jq -ce '.properties += [{"name":"apiUrl","type":"string","value":"https://play.aaaaaaaaaaaadventu.re"}] | (.layers[] | select(.properties != null and .properties[].name=="jitsiRoom") | .properties) += [{"name":"jitsiUrl","type":"string","value":"meet.jit.si"}]' $f >$tmp
+	jq -ce '
+		.properties +=
+			[{"name":"apiUrl","type":"string","value":"https://play.aaaaaaaaaaaadventu.re"}]
+		| (.layers[] | select(.properties != null and .properties[].name=="jitsiRoom") | .properties) +=
+			[{"name":"jitsiUrl","type":"string","value":"meet.jit.si"}]
+		| (.layers[].properties[]? | select(.name == "exitUrl" and (.value | startswith("world://")))| .name) |=
+			"badExitUrl"
+		' $f >$tmp
 	mv $tmp $f
 done
